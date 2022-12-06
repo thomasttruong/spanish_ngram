df95e0ed991569b1969a76b2288d2c8d7808bea3
thomasttruong <thomastruong@umass.edu>
2022-12-06 14:35:27 -0500

    tokenizer : problem with punctuation

--- spa_ngrams.py
+++ spa_ngrams.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
 with open("./sentence-collector.txt") as input_file:
-    words = [line.rstrip() for line in input_file]
+    sentences = [line.rstrip() for line in input_file]
+
+words = []
+for sentence in sentences:
+	for word in sentence.rsplit():
+		words.append(word)
+
+
+print(words)
 
