e1bfd191bad275a27f7345f7a80c892aa922f936
Frescodigo <thescrambler140@gmail.com>
2022-12-06 15:22:35 -0500

    tokenizes and now removes punctuation

--- spa_ngrams.py
+++ spa_ngrams.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
+import epitran
+import string
+
+epi = epitran.Epitran('spa-Latn')
+punct = ".,!¡?¿\"\';:"
+
 with open("./sentence-collector.txt") as input_file:
     sentences = [line.rstrip() for line in input_file]
 
 words = []
 for sentence in sentences:
-	for word in sentence.rsplit():
-		words.append(word)
+    for word in sentence.rsplit():
+        # remove punctuation
+        new_word = word.translate(str.maketrans('', '', punct))
+        words.append(new_word)
 
 
 print(words)
-print('test')
 
