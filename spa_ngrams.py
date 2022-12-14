588385a370b6ff77f0815f8b25a24469118e27fc
Frescodigo <thescrambler140@gmail.com>
2022-12-14 14:48:47 -0500

    i have added something, i am not sure what

--- spa_ngrams.py
+++ spa_ngrams.py
@@ -7,16 +7,19 @@ import numpy as np
 import random
 
 words = []
+terminator = 'ϵ' 
 
 def main():
+    # initialize
     epi = epitran.Epitran('spa-Latn')
-    punct = "-«».,!¡?¿\"\';:\u200b“”\xad…&_"
+    punct = "\t-«».,!¡?¿\"\';:\u200b“”\xad…&_"
 
     # read input file
     with open("data/sentence-collector.txt") as input_file:
         sentences = [line.rstrip() for line in input_file]
 
     # make a list of all words
+    out_file = open("data/transcribed.txt", "w")
     for sentence in sentences:
         for word in sentence.rsplit():
             # remove punctuation
@@ -25,7 +28,13 @@ def main():
             phonemic_word = epi.transliterate(new_word)
 
             words.append(phonemic_word)
+            out_file.write(phonemic_word+'\n')
+    out_file.close()
 
+    # create sorted dictionaries of
+    # unigrams, bigrams, and trigrams
+    # where keys are tuples ('a', 'b', 'c')
+    # and values are frequencies
     unigram_dict = make_ngram_dict(1)
     bigram_dict  = make_ngram_dict(2)
     trigram_dict = make_ngram_dict(3)
@@ -34,7 +43,11 @@ def main():
     # make_frequency_plot(bigram_dict, 'Bigram')
     # make_frequency_plot(trigram_dict, 'Trigram')
 
-    print(random_bigram_word(bigram_dict, 6))
+
+    # prints 3 random words using bigram dictionary
+    # of length 8
+    for j in range (3):
+        print(random_bigram_word(bigram_dict, 8))
 
 def make_ngram_dict(n):
     dict_ngrams = dict()
@@ -42,7 +55,7 @@ def make_ngram_dict(n):
     # change number of boundary symbols depending on n-gram
     boundary_symbol = ''
     for i in range(n-1):
-        boundary_symbol += 'ϵ'
+        boundary_symbol += terminator
 
     # add boundary symbol to each word
     for word in words:
@@ -56,43 +69,8 @@ def make_ngram_dict(n):
 
     # sort the dictionary
     sorted_ngrams = dict(sorted(dict_ngrams.items(), key=lambda x:x[1], reverse=True))
-    for (key, value) in sorted_ngrams.items():
-        if value == 1:
-            sorted_ngrams.pop(key)
-
-    for key in sorted_keys[:4]:
-        print(key, sorted_ngrams[key])
-    
-    for key in sorted_keys[-4:]:
-        print(key, sorted_ngrams[key])
 
     return sorted_ngrams
-
-def random_bigram_word(bigram_dict, average_length):
-    '''
-    Input:
-    bigram_dict: dicitonary of bigrams by their absolute frequency
-    average_length: average length of a word in the dataset
-    delta: variation between length of words
-
-    Output:
-    A random word based on the bigram frequencies
-    '''
-
-    total_bigrams = sum(list(bigram_dict.values()))
-    frequency_map = [total_bigrams]
-    index = 0
-    for (bigram, abs_freq) in bigram_dict.items():
-        for i in range(index, index + abs_freq):
-            frequency_map[i] = bigram
-        index = index + abs_freq
-    res = ""
-    for i in range(average_length):
-        tmp = random.randint(0, total_bigrams)
-        while frequency_map[tmp][0] != res[-1] and res != "":
-            tmp = random.randint(0, total_bigrams)
-        res += frequency_map[tmp]
-    return res
 
 def make_frequency_plot(sorted_ngrams, title):
     # convert keys from tuples into strings
@@ -107,6 +85,53 @@ def make_frequency_plot(sorted_ngrams, title):
 
     plt.savefig(f'plots/spa-{title.lower()}-freq.png')
     plt.clf()
+    
+def random_bigram_word(bigram_dict, average_length):
+
+    '''
+    Input:
+    bigram_dict: dicitonary of bigrams by their absolute frequency
+    average_length: average length of a word in the dataset
+    delta: variation between length of words
+
+    Output:
+    A random word based on the bigram frequencies
+    '''
+
+    total_bigrams = sum(bigram_dict.values())
+    frequency_map = []
+    index = 0
+    for (bigram, abs_freq) in bigram_dict.items():
+        for i in range(index, index + abs_freq):
+            frequency_map.append(bigram)
+        index = index + abs_freq
+    res = ""
+    for i in range(average_length):
+        curr_bigram_pos = random.randint(0, total_bigrams)
+        if res == "":
+            res += terminator
+            while (not word_start(frequency_map[curr_bigram_pos])) or word_end(frequency_map[curr_bigram_pos]):
+                curr_bigram_pos = random.randint(0, total_bigrams)
+        elif i + 1 == average_length:
+            while frequency_map[curr_bigram_pos][0] != res[-1] or (not word_end(frequency_map[curr_bigram_pos])):
+                curr_bigram_pos = random.randint(0, total_bigrams)
+            '''while (not word_end(frequency_map[curr_bigram_pos])) and frequency_map[curr_bigram_pos][0] != res[-1]:
+                curr_bigram_pos = random.randint(0, total_bigrams)'''
+        else:
+            while frequency_map[curr_bigram_pos][0] != res[-1] or terminating(frequency_map[curr_bigram_pos]):
+                curr_bigram_pos = random.randint(0, total_bigrams)
+            
+        #print(f"Debug {frequency_map[curr_bigram_pos]}")
+        #print(f"terminating({frequency_map[curr_bigram_pos]}) = {terminating(frequency_map[curr_bigram_pos])}")
+        res += frequency_map[curr_bigram_pos][1]
+    return res
+
+def terminating(bigram):
+    return word_start(bigram) or word_end(bigram)
+def word_start(bigram):
+    return bigram[0] == terminator
+def word_end(bigram):
+    return bigram[1] == terminator
 
 if __name__=="__main__":
     main()
